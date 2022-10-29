8c75d30b51a162d5ac61ee4515a5bb5ee6076909
AnjKura <108234350+AnjKura@users.noreply.github.com>
2022-10-29 04:36:31 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -23,19 +23,6 @@ resource "aws_instance" "web_server01" {
 
 }
 
-resource "aws_instance" "web_server02" {
-  ami                    = "ami-08c40ec9ead489470"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.pri_subnet1.id
-  vpc_security_group_ids = [aws_security_group.web_ssh.id]
-
-  key_name = "newssh"
-
-  tags = {
-    "Name" : "Web-server-02"
-  }
-  
-}
 
 # VPC
 resource "aws_vpc" "test-vpc" {
@@ -55,8 +42,6 @@ resource "aws_nat_gateway" "nat_gateway_prob" {
   subnet_id     = aws_subnet.subnet1.id
 }
 
-
-
 # SUBNET 1
 resource "aws_subnet" "subnet1" {
   cidr_block              = "172.19.0.0/18"
@@ -65,13 +50,6 @@ resource "aws_subnet" "subnet1" {
   availability_zone       = data.aws_availability_zones.available.names[0]
 }
 
-# SUBNET 2
-resource "aws_subnet" "pri_subnet1" {
-  cidr_block              = "172.19.64.0/18"
-  vpc_id                  = aws_vpc.test-vpc.id
-  map_public_ip_on_launch = "false"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-}
 
 # INTERNET GATEWAY
 resource "aws_internet_gateway" "gw_1" {
@@ -93,20 +71,6 @@ resource "aws_route_table_association" "route-subnet1" {
   route_table_id = aws_route_table.route_table1.id
 }
 
-# PRIVATE ROUTE TABLE
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.test-vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway_prob.id
-  }
-}
-
-resource "aws_route_table_association" "route-subnet2" {
-  subnet_id      = aws_subnet.pri_subnet1.id
-  route_table_id = aws_route_table.route_table2.id
-}
 
 output "instance_ip" {
   value = aws_instance.web_server01.public_ip
